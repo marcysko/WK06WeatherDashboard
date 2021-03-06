da3a61ccd10b6977daa5180855e083bf22bbd702
Marcia Skousen <marciaskousen@Marcias-iMac.local>
2021-03-06 12:36:32 -0700

    add 5 day forecast info

--- assets/script.js
+++ assets/script.js
@@ -96,3 +96,17 @@ function UVI(ln,lt){
             url:queryforecastURL,
             method:"get"
         }).then(function(response){
+
+            for (i=0;i<5;i++){
+                var date= new Date((response.list[((i+1)*8)-1].dt)*1000).toLocaleDateString();
+                var icon= response.list[((i+1)*8)-1].weather[0].icon;
+                var iconsite="https://openweathermap.org/img/wn/"+icon+".png";
+                var tempK= response.list[((i+1)*8)-1].main.temp;
+                var tempF=(((tempK-273.5)*1.80)+32).toFixed(2);
+                var humidity= response.list[((i+1)*8)-1].main.humidity;
+            }
+        
+        });
+    }
+
+    
