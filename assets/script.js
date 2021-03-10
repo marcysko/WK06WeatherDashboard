109d0e30f71d713b102948e41e9bc1c85f94bfcb
Marcia Skousen <marciaskousen@Marcias-iMac.local>
2021-03-10 12:26:50 -0700

    add api calls

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
-//Declare variables for city searches
+//variable to store the searched city
 var cities = JSON.parse(localStorage.getItem("saveCity")) || [];
-var sCity = city[0] || "Paris";
+// variable declarations
+var sCity = cities[0] || "Denver";
 var pdate = moment().format('MM-DD-YYYY');
 var day0 = moment().add(0, 'days').calendar();
 var day1 = moment().add(1, 'days').calendar();
@@ -10,7 +11,7 @@ var day4 = moment().add(4, 'days').calendar();
 var futforecast;
 var latitude;
 var uvi = 0;
-var weathericon;
+var weatherIcon;
 
 // Display the present and future weather
 function showWeather() {
@@ -50,30 +51,70 @@ for (var i = 0; i < cities.length; i++) {
             localStorage.setItem("saveCity", JSON.stringify(cities));
     }
 
-// Here we create the AJAX call
-
-// Here we build the URL so we can get a data from server side.
-
-// parse the response to display the current weather including the City name. the Date and the weather icon. 
-
-//Data object from server side Api for icon property.
-
-// The date format method is taken from the  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
-
-//parse the response for name of city and concanatig the date and icon.
-
-// parse the response to display the current temperature.
-        // Convert the temp to fahrenheit
-// Display the Humidity
-
-//Display Wind speed and convert to MPH
-
-// Display UVIndex.
-        //By Geographic coordinates method and using appid and coordinates as a parameter we are going build our uv query url inside the function below
-
-  // This function returns the UVIindex response.
-  //lets build the url for uv index.
-
-  // Here we display the 5 days forecast for the current city.
+    // Displays weather and forecast for present city
+    $(document).on("click", "#cBtn", function () {
+      sCity = $(this).text()
+      preForecast()
+      futureFore()
+    })
   
-  //Set up the API key
+    //Set up the API, create an AJAX call
+    function preForecast() {
+      $.ajax({
+        url: 'https://api.openweathermap.org/data/2.5/weather?q=' + sCity + '&APPID=68cbf1341d19c8d9fe9386af25ae8a00',
+        method: "GET"
+      }).then(function (response) {
+        forecast = response  
+    weatherIcon = $("<img src=https://openweathermap.org/img/wn/" + forecast.weather[0].icon + ".png />")
+    $("#present-city").text(sCity + " - " + pdate)
+    $("#present-city").append(weatherIcon)
+    // add text for temp, humidity, and wind speed
+    $("#temperature").text("Temperature: " + ((((forecast.main.temp) - 273.15) * (9 / 5) + 32).toFixed(1)) + "°F")
+    $("#humidity").text("Humidity: " + forecast.main.humidity + "%")
+    $("#wind-speed").text("Wind Speed: " + (forecast.wind.speed) + " MPH")
+    // assign latitude and longitude for api call to get UV information
+    lat = forecast.coord.lat
+    long = forecast.coord.lon
+    $.ajax({
+      url: 'https://api.openweathermap.org/data/2.5/uvi?appid=68cbf1341d19c8d9fe9386af25ae8a00&lat=' + lat + '&lon=' + long,
+      method: "GET"
+    }).then(function (response) {
+      uvi = response.value
+        if (uvi < 3) {
+          $(".uv-index").attr("id", "low");
+        }
+        if (uvi <= 6 && uvi >=3) {
+          $(".uv-index").attr("id", "med");
+        }
+        if (uvi <= 9 && uvi >=6.01) {
+          $(".uv-index").attr("id", "high");
+        }
+        if (uvi > 9) {
+          $(".uv-index").attr("id", "v-high");
+        }
+      $(".uv-index").text(uvi)
+    })
+  })
+};
+
+// Display 5 Day forecast data
+function futureFore() {
+  $.ajax({
+    url: 'https://api.openweathermap.org/data/2.5/forecast?q=' + sCity + '&APPID=68cbf1341d19c8d9fe9386af25ae8a00',
+    method: "GET"
+  }).then(function (response) {
+    var day = response.list
+    $("#day1").text(day[2].dt_txt.split(" ")[0])
+    $("#day1Icon").html($("<img src=https://openweathermap.org/img/wn/" + day[2].weather[0].icon + ".png />"))
+    $("#day1Temp").text("Temp: " + ((((day[2].main.temp) - 273.15) * (9 / 5) + 32).toFixed(1)) + "°F")
+    $("#day1Humid").text("Humidity: " + day[2].main.humidity + "%")
+
+
+  }
+
+
+
+  )
+}
+
+    
